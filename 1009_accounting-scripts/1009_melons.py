d76eb7fca80d62d8e7d5e79c5f4d59d05cf4d09e
Meggan M Green <hackbright@meggangreen.com>
2017-10-10 01:53:35 +0000

    updated melons.py for new data and dict of dict

--- 1009_accounting-scripts/1009_melons.py
+++ 1009_accounting-scripts/1009_melons.py
@@ -1,23 +1,7 @@
-melon_names = {
-    1: "Honeydew",
-    2: "Crenshaw",
-    3: "Crane",
-    4: "Casaba",
-    5: "Cantaloupe",
-}
-
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
-
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
+melon_info = {
+    'Honeydew': {'price': 0.99, 'has_seeds': False, 'weight': None, 'flesh_color': None, 'rind_color': None},
+    'Crenshaw': {'price': 2.00, 'has_seeds': True, 'weight': None, 'flesh_color': None, 'rind_color': None},
+    'Crane': {'price': 2.50, 'has_seeds': True, 'weight': None, 'flesh_color': None, 'rind_color': None},
+    'Casaba': {'price': 2.50, 'has_seeds': True, 'weight': None, 'flesh_color': None, 'rind_color': None},
+    'Cantaloupe': {'price': 0.99, 'has_seeds': True, 'weight': None, 'flesh_color': None, 'rind_color': None}
 }
