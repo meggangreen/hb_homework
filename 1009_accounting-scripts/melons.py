2c919a2db3135b79bff997c1c4443a5764dfa189
Meggan M Green <hackbright@meggangreen.com>
2017-10-10 02:54:12 +0000

    renamed file to work in script; unclear syntax error

---
+++ 1009_accounting-scripts/melons.py
@@ -0,0 +1,7 @@
+melon_data = {
+    'Honeydew': {'price': 0.99, 'has_seeds': False, 'weight': None, 'color_flesh': None, 'color_rind': None},
+    'Crenshaw': {'price': 2.00, 'has_seeds': True, 'weight': None, 'color_flesh': None, 'color_rind': None},
+    'Crane': {'price': 2.50, 'has_seeds': True, 'weight': None, 'color_flesh': None, 'color_rind': None},
+    'Casaba': {'price': 2.50, 'has_seeds': True, 'weight': None, 'color_flesh': None, 'color_rind': None},
+    'Cantaloupe': {'price': 0.99, 'has_seeds': True, 'weight': None, 'color_flesh': None, 'color_rind': None}
+}
